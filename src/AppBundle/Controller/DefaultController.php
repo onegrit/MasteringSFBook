1d4dc10f439fbcb1d340e35caab51349e73d4897
OneGrit <2012joe@gmail.com>
2018-09-14 09:06:35 +0800

    create aboutAction Controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,14 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/about/{name}",name="aboutPage")
+     */
+    public function aboutAction($name)
+    {
+        return $this->render('about/index.html.twig',[
+            'name'  =>  $name
+        ]);
+    }
 }
